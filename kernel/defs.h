9e77133a96613a9e6be6708504ba465f7057918e
OMA LZK <OMA_LZK@outlook.com>
2026-08-29 16:35:13 +0800

    完成 Implement copy-on-write fork

--- kernel/defs.h
+++ kernel/defs.h
@@ -59,6 +59,9 @@ void            ireclaim(int);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void            incref(uint64);
+extern struct   spinlock ref_lock;
+extern int      refcount[];
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -169,6 +172,7 @@ int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 int             ismapped(pagetable_t, uint64);
 uint64          vmfault(pagetable_t, uint64, int);
+int             cow_handle(pagetable_t, uint64);
 
 // plic.c
 void            plicinit(void);
